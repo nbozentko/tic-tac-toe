0b4448ac5625eb5da43ba61577d44ee15296191c
Nick <nickbozdev@gmail.com>
2020-05-02 15:24:59 -0400

    Fix socket

--- src/client/Home/Home.jsx
+++ src/client/Home/Home.jsx
@@ -3,7 +3,7 @@ import Box from '@material-ui/core/Box';
 import socketIOClient from 'socket.io-client';
 import Board from '../Board/Board';
 
-const socket = socketIOClient('http://localhost:8080');
+const socket = socketIOClient();
 socket.on('test', data => console.log(data));
 
 export default class Home extends React.Component {
