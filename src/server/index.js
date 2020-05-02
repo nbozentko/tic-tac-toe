5ff8d0cccc581f8d7859332ca116bbeee193423b
Nick <nickbozdev@gmail.com>
2020-05-02 00:20:57 -0400

    Sockets proof of concept

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
-
+var cors = require('cors')
+var bodyParser = require('body-parser')
 const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 const router = express.Router();
 
-app.use(express.static('dist'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('dist'))
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+server.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+// Test
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit("test", "a new client has connected");
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+setInterval(() => {
+    io.emit("test", " some data :) " + counter);
+}, 3000)
+
 
 module.exports = router;
